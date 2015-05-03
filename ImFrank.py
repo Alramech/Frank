2d9bdde8611099f73866e28503c52d463e5dfe54
Yizhe Liu <yizow@berkeley.edu>
2015-05-03 01:51:51 -0700

    Rewrote network architecture. Franks are clients querying a central server with sensor data. Server can communicate with separate robots. No longer using ansible anymore.

--- ImFrank.py
+++ ImFrank.py
@@ -1,26 +1,78 @@
 from grizzly import *
 import time
-import ansible 
+import zmq
+from multiprocessing import Process
 
-addrs=Grizzly.get_all_ids()
-gs=[] 
-for addr in addrs:
-    g = Grizzly(addr)
-    g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
-    g.limit_acceleration(142)
-    g.limit_current(10)
-    gs.append(g)
 
-for g in gs:
-    g.set_target(0)
+# Client process.
+def client(port, send_msg, recv_msg):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://%s:%d" % (remoteIP, port))
+    
+    while True:
+        socket.send_json(send_msg[0])
+        send_msg[0] += 1
+        recv_msg[0] = socket.recv()
+        print recv_msg[0]
+        time.sleep(.3)
 
-while True:
-    command = ansible.recv()
-    print command
-    if command:
-        # for i in range(len(command)):
-        #     if abs(command[i]) < .15:
-        #         command[i] = 0
-        for i in range(len(gs)):
-            gs[i].set_target(100.0*command[i])
-        time.sleep(0.01)
+send_msg = [0]
+recv_msg = [None]
+
+
+# Doesn't do anything.
+def init():
+    pass
+
+# Sends a message. 
+def send(msg):
+    send_msg[0] = msg
+
+# Receives a message
+def recv():
+    return recv_msg
+
+port = 12345
+
+remoteIP = "192.168.0.100"
+
+send_process = Process(target=client, args=(port, send_msg, recv_msg))
+send_process.start()
+
+
+
+
+
+
+
+# addrs=Grizzly.get_all_ids()
+# gs=[]
+# for addr in addrs:
+#     g = Grizzly(addr)
+#     g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
+#     g.limit_acceleration(142)
+#     g.limit_current(10)
+#     gs.append(g)
+
+# for g in gs:
+#     g.set_target(0)
+
+# while True:
+#     data = 0
+#     send(data)
+#     data += 1
+#     command = ansible.recv()
+#     print command
+#     if command:
+#         print "Command!"
+#         print command
+#         # for i in range(len(command)):
+#         #     if abs(command[i]) < .15:
+#         #         command[i] = 0
+#         for i in range(len(gs)):
+#             gs[i].set_target(100.0*command[i])
+# #    else:
+# #       for i in range(len(gs)):
+# #           gs[i].set_target(0)
+#     time.sleep(.05)
--- driveFrank.py
+++ driveFrank.py
@@ -1,23 +1,74 @@
 import pygame
 import time
-import ansible
+import random
 
-pygame.init()
-pygame.joystick.init()
-j = pygame.joystick.Joystick(0)
-j.init()    
+import zmq, yaml
+from multiprocessing import Process
 
-while True:
-    pygame.event.get()
 
-    throttles = list((j.get_axis(1), -j.get_axis(4)))
+# Server process.
+def server(port, send_msg, recv_msg):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:%d" % (port,))
+    while True:
+        # print send_msg[0]
+        recv_msg[0] = socket.recv()
+        send_msg[0] += 1
+        print recv_msg[0]
+        i = 0
+        while i < 10000:
+            i += 1
+        socket.send_json(send_msg[0])
 
-    for i in range(len(throttles)):
-        if abs(throttles[i]) < .15:
-            throttles[i] = 0
-    # for i in range(len(gs)):
-    #     print gs[i].read_encoder()
-    #     gs[i].set_target(100.0*throttles[i])
-    ansible.send(throttles)
-    print throttles
-    time.sleep(.05)
+
+send_msg = [10]
+recv_msg = [None]
+
+
+# Doesn't do anything.
+def init():
+    pass
+
+# Sends a message. 
+def send(msg):
+    send_msg[0] = msg
+
+# Receives a message
+def recv():
+    return recv_msg[0]
+
+port = 12345
+
+remoteIP = "192.168.0.1"
+
+send_process = Process(target=server, args=(port, send_msg, recv_msg))
+send_process.start()
+
+
+
+
+
+
+
+
+# pygame.init()
+# pygame.joystick.init()
+# j = pygame.joystick.Joystick(0)
+# j.init()    
+
+# while True:
+#     pygame.event.get()
+
+#     throttles = list((j.get_axis(1), -j.get_axis(4)))
+
+#     for i in range(len(throttles)):
+#         if abs(throttles[i]) < .15:
+#             throttles[i] = 0
+#     # for i in range(len(gs)):
+#     #     print gs[i].read_encoder()
+#     #     gs[i].set_target(100.0*throttles[i])
+#     send(throttles)
+#     print recv()
+#     # print throttles
+#     time.sleep(.05)
