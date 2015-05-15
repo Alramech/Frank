4c50e41d65b131a06828b48f0f930523ca25b356
Yizhe Liu <yizow@berkeley.edu>
2015-05-14 23:20:48 -0700

    renabled driving

--- ImFrank.py
+++ ImFrank.py
@@ -46,33 +46,33 @@ send_process.start()
 
 
 
-# addrs=Grizzly.get_all_ids()
-# gs=[]
-# for addr in addrs:
-#     g = Grizzly(addr)
-#     g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
-#     g.limit_acceleration(142)
-#     g.limit_current(10)
-#     gs.append(g)
+addrs=Grizzly.get_all_ids()
+gs=[]
+for addr in addrs:
+    g = Grizzly(addr)
+    g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
+    g.limit_acceleration(142)
+    g.limit_current(10)
+    gs.append(g)
 
-# for g in gs:
-#     g.set_target(0)
+for g in gs:
+    g.set_target(0)
 
-# while True:
-#     data = 0
-#     send(data)
-#     data += 1
-#     command = ansible.recv()
-#     print command
-#     if command:
-#         print "Command!"
-#         print command
-#         # for i in range(len(command)):
-#         #     if abs(command[i]) < .15:
-#         #         command[i] = 0
-#         for i in range(len(gs)):
-#             gs[i].set_target(100.0*command[i])
-# #    else:
-# #       for i in range(len(gs)):
-# #           gs[i].set_target(0)
-#     time.sleep(.05)
+while True:
+    data = 0
+    send(data)
+    data += 1
+    command = ansible.recv()
+    print command
+    if command:
+        print "Command!"
+        print command
+        # for i in range(len(command)):
+        #     if abs(command[i]) < .15:
+        #         command[i] = 0
+        for i in range(len(gs)):
+            gs[i].set_target(100.0*command[i])
+#    else:
+#       for i in range(len(gs)):
+#           gs[i].set_target(0)
+    # time.sleep(.05)
--- driveFrank.py
+++ driveFrank.py
@@ -52,23 +52,23 @@ send_process.start()
 
 
 
-# pygame.init()
-# pygame.joystick.init()
-# j = pygame.joystick.Joystick(0)
-# j.init()    
+pygame.init()
+pygame.joystick.init()
+j = pygame.joystick.Joystick(0)
+j.init()    
 
-# while True:
-#     pygame.event.get()
+while True:
+    pygame.event.get()
 
-#     throttles = list((j.get_axis(1), -j.get_axis(4)))
+    throttles = list((j.get_axis(1), -j.get_axis(4)))
 
-#     for i in range(len(throttles)):
-#         if abs(throttles[i]) < .15:
-#             throttles[i] = 0
-#     # for i in range(len(gs)):
-#     #     print gs[i].read_encoder()
-#     #     gs[i].set_target(100.0*throttles[i])
-#     send(throttles)
-#     print recv()
-#     # print throttles
-#     time.sleep(.05)
+    for i in range(len(throttles)):
+        if abs(throttles[i]) < .15:
+            throttles[i] = 0
+    # for i in range(len(gs)):
+    #     print gs[i].read_encoder()
+    #     gs[i].set_target(100.0*throttles[i])
+    send(throttles)
+    print recv()
+    # print throttles
+    # time.sleep(.05)
