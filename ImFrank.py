f32d5f9db58bddb3fc61f4b94755d8c8fbf4b120
Alramech <dinhchristopher95@gmail.com>
2015-10-15 22:41:16 -0700

    changed process to thread

--- ImFrank.py
+++ ImFrank.py
@@ -6,17 +6,35 @@ from multiprocessing import Process
 
 # Client process.
 def client(port, send_msg, recv_msg):
+    leftMotor = Grizzly(0xF-0xB)
+    leftMotor.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_BRAKE)
+    leftMotor.limit_acceleration(60)
+    leftMotor.limit_current(5)
+    rightMotor = Grizzly(0xF-0x9)
+    rightMotor.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_BRAKE)
+    rightMotor.limit_acceleration(60)
+    rightMotor.limit_current(5)
+
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://%s:%d" % (remoteIP, port))
-    
+   
+    print 'Waiting for client to send.' 
     while True:
+      try:
         socket.send_json(send_msg[0])
         send_msg[0] += 1
-        recv_msg[0] = socket.recv()
+        recv_msg = socket.recv_json()
         print recv_msg[0]
-        time.sleep(.3)
-
+        recv_msg = recv_msg[0]
+        leftMotor.set_target(float(recv_msg[0])*100)
+        rightMotor.set_target(float(recv_msg[1])*100)
+        time.sleep(.05)
+      except BaseException as e:
+        print e
+        
+        time.sleep(1)
+        exit(0)
 send_msg = [0]
 recv_msg = [None]
 
@@ -37,42 +55,42 @@ port = 12345
 
 remoteIP = "192.168.0.100"
 
-send_process = Process(target=client, args=(port, send_msg, recv_msg))
-send_process.start()
+#send_process = Process(target=client, args=(port, send_msg, recv_msg))
+#send_process.start()
+
+client(port, send_msg, recv_msg)
 
 
 
 
 
+# addrs=Grizzly.get_all_ids()
+# gs=[]
+# for addr in addrs:
+#     g = Grizzly(addr)
+#     g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
+#     g.limit_acceleration(142)
+#     g.limit_current(10)
+#     gs.append(g)
 
+# for g in gs:
+#     g.set_target(0)
 
-addrs=Grizzly.get_all_ids()
-gs=[]
-for addr in addrs:
-    g = Grizzly(addr)
-    g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_COAST)
-    g.limit_acceleration(142)
-    g.limit_current(10)
-    gs.append(g)
-
-for g in gs:
-    g.set_target(0)
-
-while True:
-    data = 0
-    send(data)
-    data += 1
-    command = ansible.recv()
-    print command
-    if command:
-        print "Command!"
-        print command
-        # for i in range(len(command)):
-        #     if abs(command[i]) < .15:
-        #         command[i] = 0
-        for i in range(len(gs)):
-            gs[i].set_target(100.0*command[i])
-#    else:
-#       for i in range(len(gs)):
-#           gs[i].set_target(0)
-    # time.sleep(.05)
+# while True:
+#     data = 0
+#     send(data)
+#     data += 1
+#     command = ansible.recv()
+#     print command
+#     if command:
+#         print "Command!"
+#         print command
+#         # for i in range(len(command)):
+#         #     if abs(command[i]) < .15:
+#         #         command[i] = 0
+#         for i in range(len(gs)):
+#             gs[i].set_target(100.0*command[i])
+# #    else:
+# #       for i in range(len(gs)):
+# #           gs[i].set_target(0)
+#     time.sleep(.05)
--- driveFrank.py
+++ driveFrank.py
@@ -1,10 +1,10 @@
 import pygame
 import time
 import random
-
+import ansible
 import zmq, yaml
 from multiprocessing import Process
-
+import thread
 
 # Server process.
 def server(port, send_msg, recv_msg):
@@ -14,17 +14,23 @@ def server(port, send_msg, recv_msg):
     while True:
         # print send_msg[0]
         recv_msg[0] = socket.recv()
-        send_msg[0] += 1
+        #send_msg[0] += 1
+        send_msg_2.msg[0] += 1
         print recv_msg[0]
         i = 0
-        while i < 10000:
-            i += 1
-        socket.send_json(send_msg[0])
+        #while i < 10000:
+        #    i += 1
+        socket.send_json(send_msg)
+        time.sleep(.3)
 
-
-send_msg = [10]
+send_msg = [0,0]
 recv_msg = [None]
 
+class Holder:
+    def __init__(self, val):
+        self.msg = val
+
+send_msg_2 = Holder([0,0])
 
 # Doesn't do anything.
 def init():
@@ -39,23 +45,26 @@ def recv():
     return recv_msg[0]
 
 port = 12345
-
+x = 1
 remoteIP = "192.168.0.1"
 
-send_process = Process(target=server, args=(port, send_msg, recv_msg))
-send_process.start()
+#send_process = Process(target=server, args=(port, send_msg, recv_msg))
+#send_process.start()
+send_thread = thread.start_new_thread(server, (port, send_msg, recv_msg))
 
-
-
-
-
+"""
+while(True):
+    print send_msg, "msg"
+    print send_msg_2.msg, "msg2"
+    time.sleep(1)
+"""
 
 
 
 pygame.init()
 pygame.joystick.init()
 j = pygame.joystick.Joystick(0)
-j.init()    
+j.init()
 
 while True:
     pygame.event.get()
@@ -65,10 +74,17 @@ while True:
     for i in range(len(throttles)):
         if abs(throttles[i]) < .15:
             throttles[i] = 0
+        #if throttles[i] > .7:
+        #    throttles[i] = .7
+        #if throttles[i] < -.7:
+        #    throttles[i] = -.7
     # for i in range(len(gs)):
     #     print gs[i].read_encoder()
     #     gs[i].set_target(100.0*throttles[i])
     send(throttles)
+    print throttles
+    #ansible.send(throttles)
     print recv()
     # print throttles
-    # time.sleep(.05)
+    time.sleep(.05)
+
